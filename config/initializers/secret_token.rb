c7bf37c89d89365965eb8b60298d0660b891d494
BogomolSergo <breinkiller650@gmail.com>
2017-04-09 22:51:03 +0300

    yes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthByVideo::Application.config.secret_token = '94bc1bbcd8c4711c8cc3e09db97f0dce22415a2c78a9179630be51a46b1f45e1fb158531cb827f4d88df044501aae90d6a91823267e9050a607c82192ea6675c'
